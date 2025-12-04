0964cb015627a781e28e0f5151790d724ac9c56a
Carbonadooo <zshaoyu@connect.hku.hk>
2025-12-04 14:36:12 -0500

    Add simclr model, modify info_nce.py

--- imu_ssl/loss/info_nce.py
+++ imu_ssl/loss/info_nce.py
@@ -2,14 +2,22 @@ import torch
 import torch.nn.functional as F
 
 def info_nce(z1, z2, temperature=0.1):
-    """
-    z1, z2: (B, D)
-    """
-    B = z1.size(0)
+    batch_size = z1.shape[0]
+
     z1 = F.normalize(z1, dim=1)
     z2 = F.normalize(z2, dim=1)
 
-    logits = torch.matmul(z1, z2.T) / temperature
-    labels = torch.arange(B, device=z1.device)
+    # positive logits
+    pos = torch.sum(z1 * z2, dim=1) / temperature  # (B,)
 
-    return F.cross_entropy(logits, labels)
+    # all negatives: z1 vs z2_j for j != i
+    neg = z1 @ z2.T  # (B, B)
+    mask = torch.eye(batch_size, device=z1.device).bool()
+    neg = neg.masked_fill(mask, -9e15) / temperature
+
+    logits = torch.cat([pos.unsqueeze(1), neg], dim=1) # (B, 1+B)
+
+    labels = torch.zeros(batch_size, dtype=torch.long, device=z1.device)
+
+    loss = F.cross_entropy(logits, labels)
+    return loss
---
+++ imu_ssl/models/SimCLR_model.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+from imu_ssl.loss.info_nce import info_nce
+from imu_ssl.models.projection_head import ProjectionHead
+
+class SimCLR(nn.Module):
+    def __init__(self, encoder, proj_dim=128):
+        super().__init__()
+        self.encoder = encoder
+        D = encoder.network[-1].conv1.out_channels  # last TCN hidden dim
+
+        # SimCLR projection head
+        self.projector = ProjectionHead(D, D, proj_dim)
+
+    def forward(self, x1, x2, lambda_temp=4.0):
+        # x1, x2: (B, T, C)
+        h1 = self.encoder(x1)  # (B, T, D)
+        h2 = self.encoder(x2)
+
+        # SimCLR loss
+        # global pooling -> get (B, D)
+        z1 = h1.mean(dim=1)
+        z2 = h2.mean(dim=1)
+
+        # projection head MLP
+        z1 = self.projector(z1)  
+        z2 = self.projector(z2)
+
+        loss = info_nce(z1, z2)
+        return loss
